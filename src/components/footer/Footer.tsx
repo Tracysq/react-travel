153874d7895f2e63852e0e418379383adddd0d8a
sunqi <sunqi@xiao100.com>
2021-11-30 16:46:43 +0800

    feat: 组件化 header、footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from './Footer.module.css'
+import { Layout, Typography } from "antd";
+
+const { Footer } = Layout;
+const { Title, Text } = Typography;
+
+export const AppFooter: React.FC = () => {
+  return (
+    <Footer>
+      <Title className={styles["footer-title"]} level={3}>
+        版权所有 @ React Travel
+      </Title>
+    </Footer>
+  );
+};
