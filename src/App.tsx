2ab0e71462f7df967b1b36413b292fc12b3a012e
sunqi <sunqi@xiao100.com>
2021-11-30 17:26:06 +0800

    feat: sideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import styles from "./App.module.css";
 import { AppHeader, AppFooter } from "./components";
+import { Row, Col } from "antd";
 
 function App() {
   return (
     <div className={styles.App}>
       <AppHeader />
+      {/* 页面内容 content */}
+      <div className={styles["page-content"]}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <div style={{ background: "red" }}>多重菜单</div>
+          </Col>
+          <Col span={18}>
+            <div style={{ background: "blue" }}>走马灯</div>
+          </Col>
+        </Row>
+      </div>
       <AppFooter />
     </div>
   );
