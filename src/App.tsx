81dde2fcd0a47a81013288dcbdc8a4d3b153606a
sunqi <sunqi@xiao100.com>
2021-11-30 17:42:43 +0800

    feat: carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./App.module.css";
-import { AppHeader, AppFooter } from "./components";
+import { AppHeader, AppFooter, Carousel, SideMenu } from "./components";
 import { Row, Col } from "antd";
 
 function App() {
@@ -11,10 +11,10 @@ function App() {
       <div className={styles["page-content"]}>
         <Row style={{ marginTop: 20 }}>
           <Col span={6}>
-            <div style={{ background: "red" }}>多重菜单</div>
+            <SideMenu />
           </Col>
           <Col span={18}>
-            <div style={{ background: "blue" }}>走马灯</div>
+            <Carousel />
           </Col>
         </Row>
       </div>
